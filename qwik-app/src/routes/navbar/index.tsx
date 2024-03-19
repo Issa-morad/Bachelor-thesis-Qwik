e3ca1b345f0a9804e6947f12b046499d753d89d3
Jesper Li <Jesper.Li@jordbruksverket.se>
2024-03-19 11:52:38 +0100

    fixed a merge issue and added links to all the video/image loaders

--- qwik-app/src/routes/navbar/index.tsx
+++ qwik-app/src/routes/navbar/index.tsx
@@ -11,6 +11,14 @@ import { Link } from '@builder.io/qwik-city';
     <Link href="/">Start</Link> |
     <Link href="/text">Text</Link> |
     <Link href="/audio">Audio</Link> |
+    <Link href="/image-loader/10">Image 10</Link> |
+    <Link href="/image-loader/20">Image 20</Link> |
+    <Link href="/image-loader/50">Image 50</Link> |
+    <Link href="/image-loader/80">Image 80</Link> |
+    <Link href="/video-loader/10">Video 10</Link> |
+    <Link href="/video-loader/20">Video 20</Link> |
+    <Link href="/video-loader/50">Video 50</Link> |
+    <Link href="/video-loader/80">Video 80</Link> |
 </nav>
       </>
     );
